10cdf9ccebb63d4d74ad9722e4fadf6ee5d4f616
Eun Lee <euntastic@gmail.com>
2022-10-19 00:33:01 -0400

    readme created. Added logic for filling out the board table.

--- connect4.js
+++ connect4.js
@@ -9,150 +9,188 @@ const WIDTH = 7;
 const HEIGHT = 6;
 
 let currPlayer = 1; // active player: 1 or 2
-const board = []; // array of rows, each row is array of cells  (board[y][x])
+const board = makeBoard(WIDTH, HEIGHT); // array of rows, each row is array of cells  (board[y][x])
 
-/** makeBoard: create in-JS board structure:
- *    board = array of rows, each row is array of cells  (board[y][x])
+const rootProperty = document.querySelector(':root');
+
+/** makeBoard: create in-JS board array structure
+ *    boardArray = array of rows, each row is array of cells  (boardArray[y][x])
  */
-
-function makeBoard() {
-    // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+function makeBoard(width, height) {
+  const boardArray = [];
+  for (let i = 0; i < height; i++) boardArray.push(new Array(width));
+  return boardArray;
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
-
 function makeHtmlBoard() {
-    // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
-    const htmlBoard = document.querySelector("#board");
-    // TODO: add comment for this code
-    const top = document.createElement("tr");
-    top.setAttribute("id", "column-top");
-    top.addEventListener("click", handleClick);
+  const htmlBoard = document.querySelector('#board');
 
-    for (let i = 0; i < WIDTH; i++) {
-        const headCell = document.createElement("td");
-        headCell.setAttribute("id", "col" + i);
-        top.append(headCell);
+  // Create the top table row with:
+  //  id='column-top'
+  //  click event => handleClick function.
+  const top = document.createElement('tr');
+  top.setAttribute('id', 'column-top');
+  htmlBoard.addEventListener('click', handleClick);
+
+  // Create individual cells and append to the top table row
+  // With variable id attributes 'col-n'
+  for (let i = 0; i < WIDTH; i++) {
+    const headCell = document.createElement('td');
+    headCell.setAttribute('id', 'col-' + i);
+    top.append(headCell);
+  }
+
+  htmlBoard.append(top);
+
+  // Create individual cells in a HEIGHT x WIDTH array.
+  for (let y = 0; y < HEIGHT; y++) {
+    const row = document.createElement('tr');
+    for (let x = 0; x < WIDTH; x++) {
+      const cell = document.createElement('td');
+      cell.setAttribute('id', `cell-${y}-${x}`);
+      cell.addEventListener('mouseover', hoverPiece);
+      cell.addEventListener('mouseout', clearPiece);
+      row.append(cell);
     }
-
-    htmlBoard.append(top);
-
-    // TODO: add comment for this code
-    for (let y = 0; y < HEIGHT; y++) {
-        const row = document.createElement("tr");
-        for (let x = 0; x < WIDTH; x++) {
-            const cell = document.createElement("td");
-            cell.setAttribute("id", `${y}-${x}`);
-            cell.addEventListener("mouseover", hoverPiece);
-            cell.addEventListener("mouseout", clearPiece);
-            row.append(cell);
-        }
-        htmlBoard.append(row);
-    }
+    htmlBoard.append(row);
+  }
 }
 
 /** findSpotForCol: given column x, return top empty y (null if filled) */
-
 function findSpotForCol(x) {
-    // TODO: write the real version of this, rather than always returning 0
-    return 0;
+  // TODO: write the real version of this, rather than always returning 0
+  for (let i = HEIGHT - 1; i >= 0; i--) {
+    if (!board[i][x]) return x;
+  }
+  return null;
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
-
-function placeInTable(y, x) {
-    // TODO: make a div and insert into correct table cell
+function placeInTable(y) {
+  // TODO: make a div and insert into correct table cell
+  for (let i = HEIGHT - 1; i >= 0; i--) {
+    if (!board[i][y]) {
+      board[i][y] = currPlayer;
+      return;
+    } else {
+      console.log('Next Space');
+    }
+  }
 }
 
 /** endGame: announce game end */
-
 function endGame(msg) {
-    // TODO: pop up alert message
+  // TODO: pop up alert message
 }
-
 
 /** hoverPiece: Hover the player's piece over the respective column. */
 function hoverPiece(event) {
-    const targetElement = event.target;
-    const columnTop = document.querySelector('#col' + targetElement.id[2]);
-    columnTop.classList.toggle('piece');
+  const targetElement = event.target;
+  const columnTop = document.querySelector('#col-' + targetElement.id.split('-')[2]);
+  columnTop.classList.toggle('piece');
 }
 
 /** clearPiece: Removes the player's piece over the respective column. */
 function clearPiece(event) {
-    const targetElement = event.target;
-    const columnTop = document.querySelector('#col' + targetElement.id[2]);
-    columnTop.classList.toggle('piece');
+  const targetElement = event.target;
+  const columnTop = document.querySelector('#col-' + targetElement.id.split('-')[2]);
+  columnTop.classList.toggle('piece');
+}
+
+function setPlayerColor(player) {
+  if (player == 1) rootProperty.style.setProperty('--player-color', 'red');
+  else rootProperty.style.setProperty('--player-color', 'blue');
+}
+
+// Fills out board with color.
+// MAYBE RELOGIC THIS
+function colorBoard() {
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
+      if (board[y][x]) {
+        const cell = document.querySelector(`#cell-${y}-${x}`);
+        if (board[y][x] == 1) cell.style.backgroundColor = 'red';
+        else cell.style.backgroundColor = 'blue';
+      }
+    }
+  }
 }
 
 /** handleClick: handle click of column to play piece */
-function handleClick(evt) {
-    // get x from ID of clicked cell
-    const x = +evt.target.id;
+function handleClick(event) {
+  // get x from ID of clicked cell
+  const currElement = event.target;
+  const x = currElement.id.split('-')[2];
 
-    // get next spot in column (if none, ignore click)
-    let y = findSpotForCol(x);
-    if (y === null) {
-        return;
-    }
+  if (event.target.tagName == 'TABLE') return;
+  // get next spot in column (if none, ignore click)
+  let y = findSpotForCol(x);
+  if (y === null) return;
 
-    // place piece in board and add to HTML table
-    // TODO: add line to update in-memory board
-    placeInTable(y, x);
+  // place piece in board and add to HTML table
+  // TODO: add line to update in-memory board
+  placeInTable(y);
+  colorBoard();
+  console.log(board);
 
-    // check for win
-    if (checkForWin()) {
-        return endGame(`Player ${currPlayer} won!`);
-    }
+  // check for win
+  if (checkForWin()) {
+    return endGame(`Player ${currPlayer} won!`);
+  }
 
-    // check for tie
-    // TODO: check if all cells in board are filled; if so call, call endGame
+  // check for tie
+  // TODO: check if all cells in board are filled; if so call, call endGame
 
-    // switch players
-    // TODO: switch currPlayer 1 <-> 2
+  // switch players
+  // TODO: switch currPlayer 1 <-> 2
+  if (currPlayer === 1) currPlayer = 2;
+  else currPlayer = 1;
+  console.log(currPlayer);
+  setPlayerColor(currPlayer);
 }
 
-/** checkForWin: check board cell-by-cell for "does a win start here?" */
+/** checkForWin: check board cell-by-cell for 'does a win start here?' */
 
 function checkForWin() {
-    function _win(cells) {
-        // Check four cells to see if they're all color of current player
-        //  - cells: list of four (y, x) cells
-        //  - returns true if all are legal coordinates & all match currPlayer
+  function _win(cells) {
+    // Check four cells to see if they're all color of current player
+    //  - cells: list of four (y, x) cells
+    //  - returns true if all are legal coordinates & all match currPlayer
 
-        return cells.every(
-            ([y, x]) =>
-                y >= 0 &&
-                y < HEIGHT &&
-                x >= 0 &&
-                x < WIDTH &&
-                board[y][x] === currPlayer
-        );
+    return cells.every(
+      ([y, x]) =>
+        y >= 0 &&
+        y < HEIGHT &&
+        x >= 0 &&
+        x < WIDTH &&
+        board[y][x] === currPlayer
+    );
+  }
+
+  // TODO: read and understand this code. Add comments to help you.
+
+  for (var y = 0; y < HEIGHT; y++) {
+    for (var x = 0; x < WIDTH; x++) {
+      const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+      const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+      const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+
+      if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+        return true;
+      }
     }
-
-    // TODO: read and understand this code. Add comments to help you.
-
-    for (var y = 0; y < HEIGHT; y++) {
-        for (var x = 0; x < WIDTH; x++) {
-            const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-            const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-            const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-            const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
-
-            if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
-                return true;
-            }
-        }
-    }
+  }
 }
 
 // Style Site with added Text and Elements
 document.addEventListener('DOMContentLoaded', () => {
-    const gameDiv = document.querySelector("#game");
-    const boardDiv = document.querySelector("#board");
-    const titleHeader = document.createElement("h1");
-    titleHeader.innerText = "Gravitrips";
-    gameDiv.insertBefore(titleHeader, boardDiv);
+  const gameDiv = document.querySelector('#game');
+  const boardDiv = document.querySelector('#board');
+  const titleHeader = document.createElement('h1');
+  titleHeader.innerText = 'Gravitrips';
+  gameDiv.insertBefore(titleHeader, boardDiv);
 });
 
 makeBoard();
---
+++ readme.md
@@ -0,0 +1,87 @@
+<!-- Improved compatibility of back to top link: See: https://github.com/othneildrew/Best-README-Template/pull/73 -->
+<a name="readme-top"></a>
+<!--
+*** Thanks for checking out the Best-README-Template. If you have a suggestion
+*** that would make this better, please fork the repo and create a pull request
+*** or simply open an issue with the tag "enhancement".
+*** Don't forget to give the project a star!
+*** Thanks again! Now go create something AMAZING! :D
+-->
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/Euntastic/umgc-connect-four">
+    <img src="images/connect-four.png" alt="Logo" width="80" height="80">
+  </a>
+
+<h3 align="center">Connect Four</h3>
+
+  <p align="center">
+    A game of connect four written in HTML/CSS and JavaScript.
+    <br />
+    <a href="https://github.com/github_username/repo_name">View Demo</a>
+  </p>
+</div>
+
+
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgments">Acknowledgments</a></li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+[![Product Name Screen Shot][product-screenshot]](https://example.com)
+
+Here's a blank template to get started: To avoid retyping too much info. Do a search and replace with your text editor for the following: `github_username`, `repo_name`, `twitter_handle`, `linkedin_username`, `email_client`, `email`, `project_title`, `project_description`
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+### Built With
+
+* HTML/CSS
+* JavaScript
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- CONTACT -->
+## Contact
+
+Your Name - [@twitter_handle](https://twitter.com/twitter_handle) - email@email_client.com
+
+Project Link: [https://github.com/github_username/repo_name](https://github.com/github_username/repo_name)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+<!-- ACKNOWLEDGMENTS -->
+## Acknowledgments
+
+* [Flatiron](https://www.flaticon.com) - For the Logo/Icon.
+* []()
+* []()
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[product-screenshot]: images/screenshot.png
